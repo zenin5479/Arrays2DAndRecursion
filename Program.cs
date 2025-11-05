1693f12ab3af602bf4418a73d68b02fa154ada96
zenin5479 <zenin5479@gmail.com>
2025-11-05 09:23:56 +0300

    Update 1.4 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -65,13 +65,13 @@ namespace Arrays2DAndRecursion
          Console.WriteLine(" Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем. ");
          Console.WriteLine("1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)");
          Console.WriteLine(" ----------------------------------------------------------------------------------------------------------------------- ");
-         Console.WriteLine("введите k1:  ");
+         Console.WriteLine("Введите k1:  ");
          double k1 = Convert.ToDouble(Console.ReadLine());
-         Console.WriteLine("введите b1:  ");
+         Console.WriteLine("Введите b1:  ");
          double b1 = Convert.ToDouble(Console.ReadLine());
-         Console.WriteLine("введите k2:  ");
+         Console.WriteLine("Введите k2:  ");
          double k2 = Convert.ToDouble(Console.ReadLine());
-         Console.WriteLine("введите b2:  ");
+         Console.WriteLine("Введите b2:  ");
          double b2 = Convert.ToDouble(Console.ReadLine());
 
          PointIntersection(k1, b1, k2, b2);
