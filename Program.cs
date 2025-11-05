2b241dcb5e370769b8fd053aa7364b4c89c36451
zenin5479 <zenin5479@gmail.com>
2025-11-05 09:44:12 +0300

    Update 2.5 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -66,13 +66,13 @@ namespace Arrays2DAndRecursion
          Console.WriteLine("----------------------------------------");
          Console.WriteLine("Нахождение точки пересечения двух прямых");
          Console.WriteLine("----------------------------------------");
-         Console.WriteLine("Введите k1: ");
+         Console.Write("Введите k1: ");
          double k1 = Convert.ToDouble(Console.ReadLine());
-         Console.WriteLine("Введите b1: ");
+         Console.Write("Введите b1: ");
          double b1 = Convert.ToDouble(Console.ReadLine());
-         Console.WriteLine("Введите k2: ");
+         Console.Write("Введите k2: ");
          double k2 = Convert.ToDouble(Console.ReadLine());
-         Console.WriteLine("Введите b2: ");
+         Console.Write("Введите b2: ");
          double b2 = Convert.ToDouble(Console.ReadLine());
 
          PointIntersection(k1, b1, k2, b2);
